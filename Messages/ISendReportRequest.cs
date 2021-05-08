b9968d143cc832a43be357fb3212f19e29e3b38b
Paul DeVito <pdevito3@gmail.com>
2021-05-08 17:04:54 -0400

    create-messages

---
+++ Messages/ISendReportRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Messages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ISendReportRequest
+    {
+        Guid ReportId { get; set; }
+        string Provider { get; set; } // "email" or "fax"
+        string Target { get; set; }
+    }
+}
