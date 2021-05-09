a518d6eac31366ad5dff9b3762ef2a3f0e34cc4f
Paul DeVito <pdevito3@gmail.com>
2021-05-08 22:33:55 -0400

    added best practices for queues

--- ReportSendingConsumerService/Program.cs
+++ ReportSendingConsumerService/Program.cs
@@ -32,7 +32,16 @@ namespace ReportSendingConsumerService
 
                             cfg.ReceiveEndpoint("private-reports", re =>
                             {
+                                // turns off default fanout settings
                                 re.ConfigureConsumeTopology = false;
+
+                                // a replicated queue to provide high availability and data safety. available in RMQ 3.8+
+                                re.SetQuorumQueue();
+
+                                // enables a lazy queue for more stable cluster with better predictive performance.
+                                // Please note that you should disable lazy queues if you require really high performance, if the queues are always short, or if you have set a max-length policy.
+                                re.SetQueueArgument("declare", "lazy");
+
                                 re.Consumer<EmailConsumer>();
                                 re.Consumer<FaxConsumer>();
                                 re.Bind("send-report", e =>
@@ -43,7 +52,16 @@ namespace ReportSendingConsumerService
                             });
                             cfg.ReceiveEndpoint("public-reports", re =>
                             {
+                                // turns off default fanout settings
                                 re.ConfigureConsumeTopology = false;
+
+                                // a replicated queue to provide high availability and data safety. available in RMQ 3.8+
+                                re.SetQuorumQueue();
+
+                                // enables a lazy queue for more stable cluster with better predictive performance.
+                                // Please note that you should disable lazy queues if you require really high performance, if the queues are always short, or if you have set a max-length policy.
+                                re.SetQueueArgument("declare", "lazy");
+
                                 re.Consumer<CloudConsumer>();
                                 re.Bind("send-report", e =>
                                 {
